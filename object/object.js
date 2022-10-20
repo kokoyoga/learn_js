180cddb6fb0fc4f61203a004faea8a95e090fe0b
kokoyogaadhitya1997@gmail.com <kokoyogaadhitya1997@gmail.com>
2022-10-20 23:45:18 +0800

    tambah file js

---
+++ object/object.js
@@ -0,0 +1,16 @@
+//membuat object
+var mhs = {
+  nama: "koko yoga adhitya",
+  umur: 25,
+  ips = [3.00 , 2.25 ,3.25],
+  alamat: {
+    jalan: "perumahan bds b.3 no.37",
+    kota: "balikpapan",
+    provinsi: "kalimantan timur",
+    // koko: "saya",
+    // misal: function () {
+    //   console.log("hellow world");
+  },
+};
+
+console.log(mhs);
